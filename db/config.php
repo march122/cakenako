9591831eb7a869529b2d19a729ee8cba1ba01eac
march122 <125708774+march122@users.noreply.github.com>
2024-11-22 12:22:57 +0800

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,15 +1,15 @@
 <?php
-// config.php
 
-$host = 'localhost'; // Database host
-$user = 'root';      // Database username
-$pass = '';          // Database password
-$db   = 'cake_db';      // Database name
+    $conn = mysqli_connect(
+        getenv("DB_HOST"),
+        getenv("DB_USERNAME"),
+        getenv("DB_PASSWORD"),
+        getenv("DB_DATABASE"),
+        getenv("DB_PORT") ? : "3306"
+    );
 
-// Create connection
-$conn = mysqli_connect($host, $user, $pass, $db);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+?>
